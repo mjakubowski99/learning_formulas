981204a168a2c16d4546e32b9ee63a82c9463aaa
mjakubowski99 <michal.j@devpark.pl>
2023-05-14 20:42:47 +0200

    Work save

---
+++ ui/ColumnWidgetsManager.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from ui.state.EncodingState import EncodingState
+from PyQt5.QtWidgets import *
+from ui.ColumnButton import ColumnButton
+from ui.preprocessing.MissingValuesFillingManager import MissingValuesFillingManager
+from ui.DataframeReader import DataframeReader
+from ui.ColumnActionButton import ColumnActionButton
+
+class ColumnWidgetsManager:
+
+    def __init__(self, state: EncodingState):
+        self.state = state
+        self.dataframe_reader = None
+        self.layout = QHBoxLayout()
+        self.missing_values_filling_manager = MissingValuesFillingManager()
+
+    def make(self, dataframe_reader: DataframeReader) -> QBoxLayout:
+        self.dataframe_reader = dataframe_reader
+        df = self.dataframe_reader.df
+
+        for column in df:
+            self.layout.addLayout(self.make_for_column(df, column))
+        return self.layout
+            
+    def make_for_column(self, df: pd.DataFrame, column) -> QBoxLayout:
+        layout = QVBoxLayout()
+        self.__make_label(column, layout)
+        self.__make_drop_column_button(column, layout)
+
+        if self.state.isFillMissing():
+            self.missing_values_filling_manager.make_widgets(df, column, layout)
+
+        self.__make_histogram_widget(df, column, layout)
+
+        return layout
+        
+    def __make_label(self, column, layout: QBoxLayout) -> None:
+        self.label = QLabel("Kolumna: "+column)
+        layout.addWidget(self.label)
+
+    def __make_drop_column_button(self, column, layout: QBoxLayout) -> None:
+        drop_column = ColumnActionButton("Usuń kolumnę", column)
+        drop_column.clicked.connect(self.__on_drop_column)
+        layout.addWidget(drop_column)
+
+    def __make_histogram_widget(self, df: pd.DataFrame, column, layout: QBoxLayout):
+        button = ColumnButton(df, column, "Pokaż histogram")
+        layout.addWidget(button)
+
+    def __on_drop_column(self, button: ColumnActionButton):
+        df = self.dataframe_reader.df 
+        self.dataframe_reader.setDataframe(df.drop(columns=[button.column]))
+
+        
+
+
+
+
+    
+
+        
+        
+        
--- ui/LabeledSpinBox.py
+++ ui/LabeledSpinBox.py
@@ -1,21 +1,24 @@
 from PyQt5.QtWidgets import QSpinBox, QLabel
 
-class LabeledComboBox:
+class LabeledSpinBox:
 
     def __init__(self, label, init_value: int):
         self.__label = QLabel(label)
-        self.__combo_box = QSpinBox()
-        self.__combo_box.setValue(init_value)
+        self.__spin_box = QSpinBox()
+        self.__spin_box.setValue(init_value)
 
     def add_to_layout(self, layout):
         layout.addWidget(self.__label)
-        layout.addWidget(self.__combo_box)
+        layout.addWidget(self.__spin_box)
         return self
     
     def setValueRange(self, min:int, max: int):
-        self.__combo_box.setRange(min,max)
+        self.__spin_box.setRange(min,max)
         return self
     
+    def get_spin_box(self) -> QSpinBox:
+        return self.__spin_box
+    
     def get_value(self):
-        return self.__combo_box.value()
+        return self.__spin_box.value()
         
