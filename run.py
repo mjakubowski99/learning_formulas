ac08aed70071f04505034599e425ef4f7ec533b7
mjakubowski99 <michal.j@devpark.pl>
2023-11-24 15:58:53 +0100

    Work save

--- run.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-df = pd.read_csv('datasets/dataset5/letter-recognition.csv')
-
-print(df[df.letter.isin(['T', 'I', 'D', 'A', 'C', 'X', 'Y', 'Z'])].reset_index().to_csv('datasets/dataset5/letter-recognition1.csv'))
